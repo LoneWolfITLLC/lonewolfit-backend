2be6ef205dab3c17de55ce3c7cf5d8ad0f648f53
LoneWolfITLLC <arlo.lonewolfit@gmail.com>
2025-12-01 02:03:07 -0600

    Temp commit...

--- server.js
+++ server.js
@@ -32,6 +32,8 @@ const corsOptions = {
 				"http://localhost:3000",
 				"http://localhost:5500",
 				"http://127.0.0.1:5500",
+				"http://localhost:5501",
+				"http://localhost:5502",
 			];
 		} else if (process.env.NODE_ENV === "production") {
 			allowedOrigins = [
